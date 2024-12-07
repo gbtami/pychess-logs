6224ba4265f85302d69d43e2f0d145202acb1c45
gbtami <gbtami@gmail.com>
2024-12-07 12:27:18 +0100

    Basic log server

---
+++ server.py
@@ -0,0 +1,26 @@
+import asyncio
+import os
+import sys
+
+if sys.platform not in ("win32", "darwin"):
+    import uvloop
+else:
+    print("uvloop not installed")
+
+from aiohttp import web
+
+if sys.platform not in ("win32", "darwin"):
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+
+async def logs(request):
+    data = await request.post()
+    print("LOG POSTED:")
+    print(data)
+    return web.Response()
+
+
+if __name__ == "__main__":
+    app = web.Application()
+    app.add_routes([web.post('/', logs)])
+    web.run_app(app, access_log=None, port=int(os.environ.get("PORT", 8080)))
